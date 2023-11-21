0698f9a0fa4f3f45245bff87540db6921c187bc9
Christopher Bare <christopherbare@outlook.com>
2023-11-20 23:08:35 -0500

    fixed terraform module import

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "s3_website" {
-  source = "github.com/ChristopherBare/terraform-modules"
+  source = "github.com/ChristopherBare/terraform-modules/s3_website"
   website_bucket_name     = "url-shortener-bucket"
   index_document          = "index.html"
   error_document          = "error.html"
