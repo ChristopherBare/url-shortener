4fa4a08e0e776ca52cbecf4700c0515e91d8faf2
Christopher Bare <christopherbare@outlook.com>
2023-11-21 15:20:54 -0500

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,10 +5,3 @@ module "s3_website" {
   error_document      = "error.html"
   origin_id           = "url-short-origin"
 }
-
-resource "null_resource" "upload_files" {
-  provisioner "local-exec" {
-    command = "aws s3 cp ../url-shortener/dist s3://${module.s3_website.bucket_name}/ --recursive"
-  }
-  depends_on = [module.s3_website]
-}
