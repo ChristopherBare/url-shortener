1c18fc5edef13e81f5ec8d630ed995d5078c475c
Christopher Bare <christopherbare@outlook.com>
2023-11-20 23:31:47 -0500

    Fixed window location for any base url

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "s3_website" {
-  source = "github.com/ChristopherBare/terraform-modules/s3_website"
-  website_bucket_name     = "url-shortener-bucket"
-  index_document          = "index.html"
-  error_document          = "error.html"
-  origin_id               = "url-short-origin"
+  source              = "github.com/ChristopherBare/terraform-modules/s3_website"
+  website_bucket_name = "url-shortener-bucket"
+  index_document      = "index.html"
+  error_document      = "error.html"
+  origin_id           = "url-short-origin"
 }
 
