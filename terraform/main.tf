f95cfe438143e3e145b4a7d4413977ddad825b61
Christopher Bare <christopherbare@outlook.com>
2025-10-27 14:27:33 -0400

    terraform update for s3 module

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "s3_website" {
-  source              = "github.com/ChristopherBare/terraform-modules/s3_website"
+  source = "git::https://github.com/ChristopherBare/terraform-modules.git//s3_website"
   website_bucket_name = "url-shortener-bucket"
   index_document      = "index.html"
   error_document      = "error.html"
